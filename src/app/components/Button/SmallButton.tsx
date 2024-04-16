e07494bf4d2e88ef55f1ce6b96360948e8dcc003
kim <kimk40000@gmail.com>
2024-04-16 13:59:59 +0900

    PostDeleteIcon

---
+++ src/app/components/Button/SmallButton.tsx
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+const RoundBox = styled.button`
+  width: 36px;
+  min-width: 32px;
+  height: 36px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 3px;
+  border: none;
+  outline: 0;
+  border-radius: 5px;
+  cursor: pointer;
+
+  &:hover {
+    background: rgba(0, 0, 0, 0.07);
+  }
+
+  & svg {
+    fill: #646464;
+  }
+`;
+
+export default function SmallButton({
+  className,
+  value,
+  onClick,
+  Icon,
+}: {
+  className?: string;
+  value?: string;
+  onClick?: () => void;
+  Icon: () => JSX.Element;
+}) {
+  return (
+    <RoundBox onClick={onClick} className={className} value={value}>
+      <Icon />
+    </RoundBox>
+  );
+}
--- src/app/components/memo/Toolbar/index.tsx
+++ src/app/components/memo/Toolbar/index.tsx
@@ -1,4 +1,13 @@
+import SmallButton from 'app/components/Button/SmallButton';
+import { TitleText } from 'app/components/Text';
 import styled from 'styled-components';
+
+import { ReactComponent as PostDeleteIcon } from './assets/delete_black_24dp.svg';
+import { ReactComponent as PostAddIcon } from './assets/post_add_black_24dp.svg';
+import { ReactComponent as MakeBoldIcon } from './assets/format_bold_black_24dp.svg';
+import { ReactComponent as MakeSizeIcon } from './assets/text_fields_black_24dp.svg';
+import { ReactComponent as MakeTodo } from './assets/check_circle_black_24dp.svg';
+import { ReactComponent as MakeImageIcon } from './assets/collections_black_24dp.svg';
 
 const Box = styled.div`
   width: 100%;
@@ -10,6 +19,39 @@ const Box = styled.div`
   border-bottom: 1px solid #e9e9e9;
 `;
 
+const Menu = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  & div {
+    display: flex;
+  }
+`;
+
+const LeftMenu = styled(Menu)`
+  width: 300px;
+  height: 100%;
+  background-color: #fff;
+  border-right: 1px solid #e9e9e9;
+  padding: 0 10px;
+`;
+
+const RightMenu = styled(Menu)`
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+  padding: 0 10px;
+`;
+
 export default function MemoToolBar() {
-  return <Box></Box>;
+  return (
+    <Box>
+      <LeftMenu>
+        <TitleText style={{ marginLeft: '5px' }}>Memo</TitleText>
+        <SmallButton onClick={() => {}} Icon={() => <PostDeleteIcon />} />
+      </LeftMenu>
+      <RightMenu></RightMenu>
+    </Box>
+  );
 }
